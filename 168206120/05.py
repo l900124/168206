648036970b2a296c6ee4d8c645305b3b5d18c78a
l900124 <44491683+l900124@users.noreply.github.com>
2018-11-06 13:51:59 +0800

    Update 05.py

--- 168206120/05.py
+++ 168206120/05.py
@@ -1,19 +1,16 @@
-def quick_sort(alist, l, r):
-   if l < r:
-       q = partition(alist, l, r)
-       return quick_sort(alist, l, q - 1)
-       return quick_sort(alist, q + 1, r)
-   return alist
+def findSmallest(arr):
+    smallest = arr[0] 
+    smallest_index = 0 
 
-def partition(alist, l, r):
-    x = alist[r]
-    i = l - 1
-    for j in range(l, r):
-        if alist[j] <= x:
-            i += 1
-            alist[i], alist[j] = alist[j], alist[i]
-    alist[i + 1], alist[r] = alist[r], alist[i+1]
-    return i + 1
+    for i in range(1,len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
 
-alist = [21, 4, 23, 3, 2, 11, 65, 22, 8]
-print(quick_sort(alist,0,len(alist)-1))
+def selectionSort(arr): 
+    for i in range(len(arr))
+        smallest = findSmallest(arr[0:len(arr)-i])  
+        arr.append(arr.pop(smallest))              
+    return arr                                      
+print (selectionSort([5,3,8,4,10]))
